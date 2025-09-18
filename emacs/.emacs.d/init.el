a4cd5e9b4e7736339693b2a8b9375d4cd9d540e9
Richard Fieldsend <richardfieldsend@gmail.com>
2025-09-18 23:32:07 +0100

    Add notes on Makefile editing support.

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -4,7 +4,10 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages nil))
+ '(package-selected-packages
+   '(auctex catppuccin-theme darkroom dired-git-info doom-modeline ebib
+	    emmet-mode flycheck focus helm magit olivetti org-bullets
+	    rainbow-delimiters writeroom-mode)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
